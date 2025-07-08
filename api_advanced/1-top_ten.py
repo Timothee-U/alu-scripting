e96a6e0678803441aa4c768fea9831f0ce34444c
Timothee-U <t.uwayesu@alustudent.com>
2025-07-08 11:29:02 +0000

    kk

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,10 +1,20 @@
+#!/usr/bin/python3
+'''
+This module contains the function top_ten
+'''
 import requests
+from sys import argv
 
 def top_ten(subreddit):
+    '''
+    Prints the titles of the top 10 hot posts for a given subreddit.
+    If the subreddit is invalid or inaccessible, prints None.
+    '''
     headers = {'User-Agent': 'Mozilla/5.0'}
     url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
 
     response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code != 200:
         print("None")
         return
@@ -16,3 +26,9 @@ def top_ten(subreddit):
             print(post.get('data', {}).get('title'))
     except Exception:
         print("None")
+
+if __name__ == "__main__":
+    if len(argv) == 2:
+        top_ten(argv[1])
+    else:
+        print("Usage: ./1-top_ten.py <subreddit>")
