62f37a6a26b12e004100458155673b35b62622f5
Timothee-U <t.uwayesu@alustudent.com>
2025-07-21 23:44:36 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,30 +1,24 @@
 #!/usr/bin/python3
 '''
-This module contains the function top_ten
+    this module contains the function top_ten
 '''
-
 import requests
+from sys import argv
 
 
 def top_ten(subreddit):
     '''
-    Prints the titles of the top ten hot posts for a given subreddit.
-    If the subreddit is invalid or inaccessible, prints None.
+        returns the top ten posts for a given subreddit
     '''
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code != 200:
-        print(None)
-        return
-
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
     try:
-        posts = response.json().get('data', {}).get('children', [])
-        for post in posts:
-            print(post.get('data', {}).get('title'))
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
     except Exception:
         print(None)
+
 
 if __name__ == "__main__":
     top_ten(argv[1])
