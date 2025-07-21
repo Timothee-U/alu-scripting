24eda4c5ba00252025d791825306be5d10849e16
Timothee-U <t.uwayesu@alustudent.com>
2025-07-21 23:22:22 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Script that fetch 10 hot post for a given subreddit."""
+"""This module contains functions to interact with Reddit API:
+    - top_ten(subreddit): prints top 10 hot posts titles in a subreddit
+    - get_user_info(username): prints basic info about a Reddit user"""
 import requests
 
 
