5ad261822a5fcb2491d59a18b9aa56b1471eabe5
Timothee-U <t.uwayesu@alustudent.com>
2025-07-21 23:13:21 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-'''
-    this module contains the function top_ten
-'''
+"""Script that fetch 10 hot post for a given subreddit."""
 import requests
-from sys import argv
 
 
 def top_ten(subreddit):
-    '''
-        returns the top ten posts for a given subreddit
-    '''
-    user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
-                       .format(subreddit), headers=user).json()
-    try:
-        for post in url.get('data').get('children'):
-            print(post.get('data').get('title'))
-    except Exception:
+    """Return number of subscribers if @subreddit is valid subreddit.
+    if not return 0."""
+
+    headers = {
+        'User-Agent': 'python:alx_topten:v1.0 (by JeanPhilippe2025)'
+    }
+    subreddit_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    response = requests.get(subreddit_url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        json_data = response.json()
+        posts = json_data.get('data', {}).get('children', [])
+
+        for i in range(min(10, len(posts))):
+            print(posts[i].get('data', {}).get('title'))
+    else:
         print(None)
-
-
-if __name__ == "__main__":
-    top_ten(argv[1])
