a558de1f7590f34831bef01a1f202234c163d2bb
Timothee-U <t.uwayesu@alustudent.com>
2025-07-21 22:48:38 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
 '''
-    this module contains the function top_ten
+    This module contains the function top_ten
 '''
 import requests
-from sys import argv
 
 
 def top_ten(subreddit):
     '''
-        returns the top ten posts for a given subreddit
+    Prints the titles of the top 10 hot posts for a given subreddit
     '''
-    user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
-                       .format(subreddit), headers=user).json()
-    try:
-        for post in url.get('data').get('children'):
-            print(post.get('data').get('title'))
-    except Exception:
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
         print(None)
+        return
 
-
-if __name__ == "__main__":
-    top_ten(argv[1])
+    data = response.json()
+    try:
+        for post in data['data']['children']:
+            print(post['data']['title'])
+    except (KeyError, TypeError):
+        print(None)
